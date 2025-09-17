7a48602cd7ad78f53292f7da3aad75242374c001
Carlos Loría Rodríguez <carlosloriar5@gmail.com>
2025-09-16 20:12:53 -0700

    Added exercise-4.js

---
+++ js/exercise-4.js
@@ -0,0 +1,9 @@
+let n1 = 15;
+let n2 = 25;
+let n3 = 35;
+
+if (n1 + n2 > n3 && n1 + n3 > n2 && n2 + n3 > n1) {
+    console.log("It's possible");
+} else {
+    console.log("It's not possible");
+}
