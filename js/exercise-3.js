306e9dec9677c40b38a6dd9931b0e2ab01a60418
Carlos Loría Rodríguez <carlosloriar5@gmail.com>
2025-09-16 20:23:58 -0700

    update exercise-3.js

--- js/exercise-3.js
+++ js/exercise-3.js
@@ -2,10 +2,14 @@ let n1 = 15;
 let n2 = 25;
 let n3 = 35;
 
-if (n1 > n2 && n1 > n3) {
-    console.log(n1);
-} else if (n2 > n1 && n2 > n3) {
-    console.log(n2);
+if (n1 > n2) {
+    if (n1 > n3) {
+        console.log(n1);
+    }
+} else if (n2 > n1) {
+    if (n2 > n3) {
+        console.log(n2);
+    }
 } else {
     console.log(n3);
 }
