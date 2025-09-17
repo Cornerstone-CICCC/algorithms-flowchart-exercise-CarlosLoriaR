ee3d0a64fc5ba26c7fa52decb4d164a04c66cdf4
Carlos Loría Rodríguez <carlosloriar5@gmail.com>
2025-09-16 20:12:58 -0700

    Added exercise-5.js

---
+++ js/exercise-5.js
@@ -0,0 +1,12 @@
+let name = "Carlos"
+let marks = 95
+
+if (marks >= 80) {
+    console.log("Grade A");
+} else if (marks >= 60) {
+    console.log("Grade B");
+} else if (marks >= 40) {
+    console.log("Grade C");
+} else {
+    console.log("No Grade");
+}
